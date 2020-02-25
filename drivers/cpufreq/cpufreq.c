80235de0a075891065a6e0d2d494528ec2c745c0
The~Skater~187 <Theskaterdad187@gmail.com>
2020-02-24 22:26:04 -0500

    Remove KT AutoHotplug

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -33,11 +33,6 @@
 
 #include <trace/events/power.h>
 
-static unsigned int Lenable_auto_hotplug = 0;
-extern ssize_t show_auto_hotplug_enable_core_loads(struct cpufreq_policy *policy, char *buf);
-extern ssize_t store_auto_hotplug_enable_core_loads(struct cpufreq_policy *policy, const char *buf, size_t count);
-extern void apenable_auto_hotplug(bool state);
-
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
  * level driver of CPUFreq support, and its spinlock. This lock
@@ -668,21 +663,6 @@ static ssize_t show_bios_limit(struct cpufreq_policy *policy, char *buf)
 	return sprintf(buf, "%u\n", policy->cpuinfo.max_freq);
 }
 
-static ssize_t show_enable_auto_hotplug(struct cpufreq_policy *policy, char *buf)
-{
-	return sprintf(buf, "%u\n", Lenable_auto_hotplug);
-}
-static ssize_t store_enable_auto_hotplug(struct cpufreq_policy *policy,
-					const char *buf, size_t count)
-{
-	unsigned int val = 0;
-	unsigned int ret;
-	ret = sscanf(buf, "%u", &val);
-	Lenable_auto_hotplug = val;
-	apenable_auto_hotplug((bool) Lenable_auto_hotplug);
-	return count;
-}
-
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 extern ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf);
 extern ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
@@ -722,8 +702,6 @@ cpufreq_freq_attr_rw(UV_mV_table);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
-cpufreq_freq_attr_rw(enable_auto_hotplug);
-cpufreq_freq_attr_rw(auto_hotplug_enable_core_loads);
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -741,8 +719,6 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
-    &enable_auto_hotplug.attr,
-    &auto_hotplug_enable_core_loads.attr,
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 	&UV_mV_table.attr,
 #endif
