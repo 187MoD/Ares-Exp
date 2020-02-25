00c1af48a661e27c72d3b1bfaffec45b5f223bcc
The~Skater~187 <Theskaterdad187@gmail.com>
2020-02-24 22:22:37 -0500

    Remove KThermal

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -38,10 +38,6 @@ extern ssize_t show_auto_hotplug_enable_core_loads(struct cpufreq_policy *policy
 extern ssize_t store_auto_hotplug_enable_core_loads(struct cpufreq_policy *policy, const char *buf, size_t count);
 extern void apenable_auto_hotplug(bool state);
 
-//Kthermal limit holder to stop govs from setting CPU speed higher than the thermal limit
-struct cpufreq_policy trmlpolicy[10];
-unsigned int kthermal_limit = 0;
-
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
  * level driver of CPUFreq support, and its spinlock. This lock
@@ -1620,17 +1616,6 @@ EXPORT_SYMBOL(cpufreq_unregister_notifier);
  *                              GOVERNORS                            *
  *********************************************************************/
 
-
-void do_kthermal(unsigned int cpu, unsigned int freq)
-{
-	kthermal_limit = freq;
-  	if (freq > 0)
-  	{
-  		//pr_alert("DO KTHERMAL %u-%u\n", cpu, freq);
-		__cpufreq_driver_target(&trmlpolicy[cpu], freq, CPUFREQ_RELATION_H);
-	}
-}
-
 int __cpufreq_driver_target(struct cpufreq_policy *policy,
 			    unsigned int target_freq,
 			    unsigned int relation)
@@ -1639,9 +1624,6 @@ int __cpufreq_driver_target(struct cpufreq_policy *policy,
 
 	if (cpufreq_disabled())
 		return -ENODEV;
-
-    if (kthermal_limit > 0 && target_freq > kthermal_limit)
-		target_freq = kthermal_limit;
 
 	pr_debug("target for CPU %u: %u kHz, relation %u\n", policy->cpu,
 		target_freq, relation);
@@ -1945,8 +1927,6 @@ static int __cpufreq_set_policy(struct cpufreq_policy *data,
 		pr_debug("governor: change or update limits\n");
 		__cpufreq_governor(data, CPUFREQ_GOV_LIMITS);
 	}
-    
-    memcpy(&trmlpolicy[policy->cpu], policy, sizeof(struct cpufreq_policy));
 
 error_out:
 	return ret;
